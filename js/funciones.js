30b94db1fdfce6aafa9250477118bf0b53e1abb4
SchatzM <bri@npass.us>
2020-05-12 14:20:44 +0200

    Añadidas funciones.

--- js/funciones.js
+++ js/funciones.js
@@ -1,2 +1,27 @@
 'use strict';
+var contador = 0;
 
+// Función básica sin parámetros
+function hola () {
+    return 'hola';
+}
+
+// Función básica con parámetros
+function multiplica (a, b) {
+    return Number(a) * Number(b);
+}
+
+// Función con parámetros y condicional
+function sonIguales (a, b) {
+    return (a === b) ? true : false;
+}
+
+// función con dependencia de variable global
+function aumentarContador () {
+    return contador++;
+}
+
+// Función que concatena y devuelve los parámetros recibidos con un salto de línea en medio
+function dosLineas (a, b) {
+    return a + '\n' + b;
+}
